a022a44e52ee3085ac448a7e57667a5f120942c3
zhl355 <zhl355@eng.ucsd.edu>
2017-09-03 18:01:28 -0700

    a lot change ?

---
+++ lib/printmovie.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+void printmovie(){
+    printf("Guess a movie!\n");
+}
---
+++ lib/printmovie.h
@@ -0,0 +1,6 @@
+#ifndef printmovie_h
+#define printmovie_h
+	void printmovie();
+
+	
+#endif /*printmovie_h*/
